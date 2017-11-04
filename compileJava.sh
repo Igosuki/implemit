685cc50858ed5d1bf71bf0ff10e222b9781ffb67
Igosuki <igosuki+github@gmail.com>
2017-11-04 14:34:33 +0100

    Reorganize

--- compileJava.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/bash
-find src -name \*.java -print > file.list
-javac @file.list -d compiled
-cd compiled
-find ./* -name \*.class -print > ../compiled.list
-jar cvf ../code.jar @../compiled.list
